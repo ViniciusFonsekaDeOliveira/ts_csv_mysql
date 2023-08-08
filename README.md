af941da0684b136ae3a8f6269d1624fb29d1ff47
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-08-08 15:48:55 -0300

    Csv Data to Mysql Database Persistence

---
+++ README.md
@@ -0,0 +1,5 @@
+# ts_csv_mysql
+
+## Descrição
+
+Objetivo: importar qualquer quantidade de dados presentes em um arquivo .csv e persistí-los numa base de dados mysql usando uma orm.
--- index.ts
+++ index.ts
@@ -1,5 +1,54 @@
-function main() {
-  console.log("Hello World!");
+import * as readline from "readline";
+import fs from "fs";
+import csvParser from "csv-parser";
+import { PrismaClient } from "@prisma/client";
+
+function main(): number {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question("Digite o path do arquivo: ", async (myCsvFile) => {
+    console.log(`Processando ${myCsvFile}...`);
+    await importAndPersistCsvFile(myCsvFile);
+    rl.close();
+  });
+
+  return 1;
 }
+
+const importAndPersistCsvFile = async (file: string) => {
+  const prisma = new PrismaClient();
+  const header = "id;firstname;lastname;email;email2;profession";
+  const separator = ";";
+  await prisma.$connect();
+  fs.createReadStream(file)
+    .pipe(csvParser())
+    .on("data", async (row) => {
+      const transaction = row[header].split(separator);
+      try {
+        await prisma.user.create({
+          data: {
+            id: Number(transaction[0]),
+            firstname: transaction[1],
+            lastname: transaction[2],
+            email: transaction[3],
+            email2: transaction[4],
+            profession: transaction[5],
+          },
+        });
+      } catch (err) {
+        console.log(
+          "Ocorreu um erro desconhecido durante a persistência do arquivo.",
+          err
+        );
+      }
+    })
+    .on("end", async () => {
+      console.log("Arquvio csv importado e persistido com sucesso!");
+      await prisma.$disconnect();
+    });
+};
 
 main();
